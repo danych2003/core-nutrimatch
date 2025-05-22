e96b853c175e65637dea7a01ca148ec30ced12a9
Daniil Nychyporuk <danychit@outlook.com>
2025-05-23 02:19:26 +0300

    deployed

--- src/main/java/ee/danych/nutrimatch/config/WebConfig.java
+++ src/main/java/ee/danych/nutrimatch/config/WebConfig.java
@@ -1,5 +1,6 @@
 package ee.danych.nutrimatch.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${nutrimatch.frontend.url}")
+    private String FRONTEND_URL;
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(FRONTEND_URL)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("X-Available-Pages", "*")
                 .allowCredentials(true)
